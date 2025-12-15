9517671444d97b7c8d6a785b6e78cd89736cf193
Ashton Montgomery <ashtonmontgomery96@gmail.com>
2025-12-15 13:08:18 -0600

    Implemented useState for project card funtionality. Project links are initially hidden until project is clicked on. Clicking on the component swaps status of showContent variable.

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import {useState} from "react";
 
 
 function ProjectCard(props) {
-
+    const [showContent, setShowContent] = useState(false);
     const handleMouseMove = (e) => {
         const rect = e.currentTarget.getBoundingClientRect();
         const x = e.clientX - rect.left;       
@@ -11,14 +11,18 @@ function ProjectCard(props) {
     };
 
     return (
-        <div className="project group glassCard w-1/4 max-w-3xs flex flex-col justify-between items-center p-3 bg-[var(--surface-color)] rounded-lg border-[var(--border-color)] shadow-md shadow-[var(--surface-shadow-color)] hover:shadow-lg hover:shadow-[var(--surface-shadow-hovered-color)]">
+        <div className="project group glassCard relative w-1/4 max-w-3xs flex flex-col justify-evenly items-center p-3 bg-[var(--surface-color)] rounded-lg border-[var(--border-color)] shadow-md shadow-[var(--surface-shadow-color)] hover:shadow-lg hover:shadow-[var(--surface-shadow-hovered-color)]">
             <h3 className="text-lg font-bold" >{props.projectName}</h3>
             <p class="projectDescription" className="w-9/10">{props.description}</p>
-            <div className="projectLinks w-fit flex flex-col justify-center items-center my-1 gap-1 mt-2">
-                <a href={props.projectLink} className="bg-[var(--surface-color)]  w-full px-2 py-1 rounded-lg border border-white/25 focus:outline-none focus:ring-1 focus:ring-[var(--focus-ring)] focus:ring-offset-2 focus:ring-offset-[var(--ring-offset-color)]" onMouseMove={handleMouseMove}>Go to the Project</a>
-                <a href={props.githubLink} className="bg-[var(--surface-color)]  w-full px-2 py-1 rounded-lg border border-white/25 focus:outline-none focus:ring-1 focus:ring-[var(--focus-ring)] focus:ring-offset-2 focus:ring-offset-[var(--ring-offset-color)]" onMouseMove={handleMouseMove}>Go to the Project Github</a>
-            </div>
-            <div className="clickable"></div>
+            {(showContent) && (
+                <>
+                    <div className="projectLinks w-fit flex flex-col justify-center items-center my-1 gap-1 mt-2 z-10">
+                        <a href={props.projectLink} className="bg-[var(--surface-color)]  w-full px-2 py-1 rounded-lg border border-white/25 focus:outline-none focus:ring-1 focus:ring-[var(--focus-ring)] focus:ring-offset-2 focus:ring-offset-[var(--ring-offset-color)]" onMouseMove={handleMouseMove}>Go to the Project</a>
+                        <a href={props.githubLink} className="bg-[var(--surface-color)]  w-full px-2 py-1 rounded-lg border border-white/25 focus:outline-none focus:ring-1 focus:ring-[var(--focus-ring)] focus:ring-offset-2 focus:ring-offset-[var(--ring-offset-color)]" onMouseMove={handleMouseMove}>Go to the Project Github</a>
+                    </div>
+                </>
+            )} 
+            <div className="clickable absolute cursor-pointer" onClick={() => {setShowContent(!showContent); console.log("Clicked");}}></div>
         </div>
     )
 }
