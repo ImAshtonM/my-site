8e1b2a4750a6272e1632e05c7ef59a61f6877c65
Ashton Montgomery <ashtonmontgomery96@gmail.com>
2025-12-15 22:19:45 -0600

    Introduction of TechStack component. Restructured project section to have filtered subsections. One for WordPress, UI/UX Expirements for 3D CSS projects. Filtered by mapping projects and only returning ones that had matching tags values.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Projects from "./Projects"
 import ProjectCard from './ProjectCard'
 import Header from './Header'
 import Hero from './Hero'
+import TechStack from './TechStack'
 
 function App() {
   const [count, setCount] = useState(0);
@@ -24,19 +25,63 @@ function App() {
           heroImageAlt = "Alternate text for the hero image"
         />
         {/* Site Directions from last site design(clickable links) or modify site based on users choice? */}
-        <div id="projects" className="flex flex-wrap items-start justify-center gap-4">
-          {Projects.map((project) => {
-            return (
-              <ProjectCard
-              projectName= {project.name}
-              description= {project.description}
-              projectLink= {project.projectLink}
-              githubLink= {project.githubLink}
-              />
-            )
-          })}
+        <div id="projects" className="flex flex-col items-start gap-4">
+          <div id="wordPress" className="flex flex-col gap-4">
+            <h3 className="text-left text-xl font-bold ">WordPress Projects</h3>
+            <div className="flex flex-wrap items-start justify-start gap-4">
+              {Projects.map((project) => {
+                if (project["tags"].includes("WordPress")) {
+                  return (
+                    <ProjectCard
+                    projectName= {project.name}
+                    description= {project.description}
+                    projectLink= {project.projectLink}
+                    githubLink= {project.githubLink}
+                    />
+                  )
+                }
+              })}
+            </div>
+          </div>
+          <div id="uiExperiments" className="flex flex-col gap-4">
+            <h3 className="text-left text-xl font-bold ">UI/UX Experiments: 3D CSS</h3>
+            <div className="flex flex-wrap items-start justify-start gap-4">
+              {Projects.map((project) => {
+                if (project["tags"].includes("3D CSS")) {
+                  return (
+                    <ProjectCard
+                    projectName= {project.name}
+                    description= {project.description}
+                    projectLink= {project.projectLink}
+                    githubLink= {project.githubLink}
+                    />
+                  )
+                }
+              })}
+            </div>
+          </div>
+          {/* Section below won't be needed until workout tracker app is completed */}
+          {/* <div id="fullStack" className="flex flex-col gap-4">
+            <h3 className="text-left text-xl font-bold ">Full Stack/ In Development</h3>
+            <div className="flex flex-wrap items-start justify-start gap-4">
+              {Projects.map((project) => {
+                if (project["tags"].includes("Full Stack")) {
+                  return (
+                    <ProjectCard
+                    projectName= {project.name}
+                    description= {project.description}
+                    projectLink= {project.projectLink}
+                    githubLink= {project.githubLink}
+                    />
+                  )
+                }
+              })}
+            </div>
+          </div> */}
         </div>
+        {/* Add an "Upcoming Projects" section to showcase progress in the workout tracker app. Can have blog like updates covering what's been done and new techniques learnt.  */}
         {/* Tech Stack */}
+        <TechStack />
         {/* Services */}
         {/* Contact Section */}
       </main>
