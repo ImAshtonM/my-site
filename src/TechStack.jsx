d2085c819777bc82ddc846910d06bb106dd423de
Ashton Montgomery <ashtonmontgomery96@gmail.com>
2025-12-16 22:55:57 -0600

    Classes added to each tech stack image tag. Position absolute and translated down to account for height of elements for animations.

--- src/TechStack.jsx
+++ src/TechStack.jsx
@@ -1,8 +1,8 @@
 export default function TechStack() {
     return (
-        <section id="techStack" className="w-full py-4 flex flex-col gap-4 justify-center items-center">
-                <h3 className="w-full text-left text-xl text-[var(--brand-color)]">Were You Asking About My Current Tech Stack?</h3>
-                <div className="flex gap-4">
+        <section id="techStack" className="w-full p-4 flex flex-col gap-4 justify-center items-center">
+                <h3 className="w-full text-left text-xl font-bold text-[var(--brand-color)]">Were You Asking About My Current Tech Stack?</h3>
+                <div className="flex items-align justify-center gap-10">
                     <div className="basis-1/2 text-nowrap">
                         <ul>
                             <li>HTML</li>
@@ -14,13 +14,13 @@ export default function TechStack() {
                         </ul>
                     </div>
                     <div className="basis-1/2">
-                        <div id="techStackIcons">
-                            <img src="src\assets\techStack\html-5.182x256.png" alt="HTML icon" />
-                            <img src="src\assets\techStack\css-3.182x256.png" alt="CSS icon" />
-                            <img src="src\assets\techStack\javascript.256x256.png" alt="JavaScript icon" />
-                            <img src="" alt="React icon" />
-                            <img src="src\assets\techStack\nodejs.256x157.png" alt="NodeJS icon" />
-                            <img src="src\assets\techStack\postgresql-plain-wordmark.256x254.png" alt="PostgresSQL icon" />
+                        <div id="techStackIcons" className="relative transform-3d lg:translate-y-[100%] lg:translate-x-[50%]">
+                            <img src="src\assets\techStack\html-5.182x256.png" alt="HTML icon" className="absolute w-[50px] translate-y-[75%]"/>
+                            <img src="src\assets\techStack\css-3.182x256.png" alt="CSS icon" className="absolute w-[50px] translate-y-[75%]"/>
+                            <img src="src\assets\techStack\javascript.256x256.png" alt="JavaScript icon" className="absolute w-[50px] translate-y-[100%]"/>
+                            <img src="src\assets\techStack\nodejs.256x157.png" alt="NodeJS icon" className="absolute w-[50px] translate-y-[100%]"/>
+                            <img src="src\assets\techStack\postgresql-plain-wordmark.256x254.png" alt="PostgresSQL icon" className="absolute w-[50px] translate-y-[100%]"/>
+                            <img src="" alt="React icon" /* className="absolute w-[50px]" *//>
                         </div>
                     </div>
                 </div>
