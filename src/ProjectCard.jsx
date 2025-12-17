5c6051225766b087ad463f35a30a25a18be2d127
Ashton Montgomery <ashtonmontgomery96@gmail.com>
2025-12-16 22:51:38 -0600

    Moved project description to also be hidden until project card is clicked.

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -13,9 +13,9 @@ export default function ProjectCard(props) {
     return (
         <div className="project group glassCard relative w-1/4 max-w-3xs flex flex-col justify-evenly items-center p-3 bg-[var(--surface-color)] rounded-lg border-[var(--border-color)] shadow-md shadow-[var(--surface-shadow-color)] hover:shadow-lg hover:shadow-[var(--surface-shadow-hovered-color)]">
             <h3 className="text-lg font-bold" >{props.projectName}</h3>
-            <p class="projectDescription" className="w-9/10">{props.description}</p>
             {(showContent) && (
                 <>
+                <p class="projectDescription" className="w-9/10">{props.description}</p>
                     <div className="projectLinks w-fit flex flex-col justify-center items-center my-1 gap-1 mt-2 z-10">
                         <a href={props.projectLink} className="bg-[var(--surface-color)]  w-full px-2 py-1 rounded-lg border border-white/25 focus:outline-none focus:ring-1 focus:ring-[var(--focus-ring)] focus:ring-offset-2 focus:ring-offset-[var(--ring-offset-color)]" onMouseMove={handleMouseMove}>Go to the Project</a>
                         <a href={props.githubLink} className="bg-[var(--surface-color)]  w-full px-2 py-1 rounded-lg border border-white/25 focus:outline-none focus:ring-1 focus:ring-[var(--focus-ring)] focus:ring-offset-2 focus:ring-offset-[var(--ring-offset-color)]" onMouseMove={handleMouseMove}>Go to the Project Github</a>
