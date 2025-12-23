9c55f240ba4488444dd41ba17ce092011617944f
Ashton Montgomery <ashtonmontgomery96@gmail.com>
2025-12-23 12:29:39 -0600

    Updated copyright date to a variable that shows current year. Inlcuded Freepik links for icons. Changed footer tagline to Independent Web Developer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,11 +1,17 @@
 export default function Footer() {
+    const date = new Date
+    const year = date.getFullYear()
     return (
         <footer id="footer" className="w-full flex flex-col justify-evenly items-center py-2">
-            <p>Copyright © 2025 | A Montgomery Design and Development</p>
+            <p>© {year} Ashton Montgomery · Independent Web Developer</p>
             <div className="flex justify-center gap-4 hover:(--hover-color)">
                 <a href="">Privacy Policy</a>
                 <a href="">Terms & Conditions</a>
             </div>
+            <ul className="list-none text-xs">Icons provided by Freepik - Flaticon.com 
+                <li><a href="https://www.flaticon.com/free-icons/html" title="html icons">Html icons created by Freepik - Flaticon</a></li>
+                <li><a href="https://www.flaticon.com/free-icons/react" title="react icons">React icons created by Freepik - Flaticon</a></li>
+            </ul>
         </footer>
     )
 };
