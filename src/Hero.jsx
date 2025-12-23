4962a9f913c9598180e3a898e7f88ce2a1d748dc
Ashton Montgomery <ashtonmontgomery96@gmail.com>
2025-12-23 12:37:41 -0600

    Added background image, color, and blended them together. Added a support line for underneath our subtitle. Currently only used on W2 mode. Changed sizing of each of the text elements, capitalized the page title, and adjusted spacing between elements.

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,16 +1,17 @@
 export default function Hero(props) {
     return (
-        <div id="hero" className="w-full my-2 min-h-[25dvh] flex justify-center items-center">
-            <div className="container w-[80%] h-full flex justify-center items-center gap-2">
-                <div className="basis-1/2 justify-items-start">
-                    <h1 className="text-2xl">{props.pageTitle}</h1>
-                    <h2 className="text-xl">{props.pageSubTitle}</h2>
-                    <div className="cta">
+        <div id="hero" className="w-full my-2 py-4 md:min-h-[40dvh] flex justify-center items-center bg-[url(src/assets/hero4.jpg)] bg-[var(--mask-color)] bg-fixed bg-contain bg-bottom bg-blend-multiply">
+            <div className="container w-[80%] h-full flex flex-col md:flex-row justify-center items-center gap-4 relative">
+                <div className="md:basis-1/2 basis-full flex flex-col justify-items-center md:justify-items-start md:text-left gap-4">
+                    <h1 className="text-2xl capitalize">{props.pageTitle}</h1>
+                    <h2 className="text-lg">{props.pageSubTitle}</h2>
+                    <p className="text-sm">{props.support}</p>
+                    <div className="cta flex flex-col gap-2">
                         <p>{props.heroCTA}</p>
-                        <button><a href={props.heroCTALink}>Link to form</a></button>
+                        <a href={props.heroCTALink} className="w-fit rounded-md px-3 py-1 border hover:underline hover:text-[var(--hover-color)] focus:outline-none focus:ring-2 focus:ring-offset-2">{props.ctaLinkText}</a>
                     </div>
                 </div>
-                <div className="basis-1/2">
+                <div className="md:basis-1/2 basis-full">
                     <img src={props.heroImage} alt={props.heroImageAlt} srcset=""  className=""/>
                 </div>
             </div>
