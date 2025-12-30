251e7f025d69390bca2aa8cc0b64ab37b421078b
Ashton Montgomery <ashtonmontgomery96@gmail.com>
2025-12-29 18:41:31 -0600

    Updated styling. Removed unused container class and removed width contraint of 80%.

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,9 +1,9 @@
 export default function Hero(props) {
     return (
-        <div id="hero" className="w-full my-2 py-4 md:min-h-[40dvh] flex justify-center items-center bg-[url(src/assets/hero4.jpg)] bg-[var(--mask-color)] bg-fixed bg-contain bg-bottom bg-blend-multiply">
-            <div className="container w-[80%] h-full flex flex-col md:flex-row justify-center items-center gap-4 relative">
+        <div id="hero" className="w-full my-2 p-2 md:min-h-[40dvh] flex justify-center items-center bg-[url(src/assets/hero4.jpg)] bg-[var(--mask-color)] bg-fixed bg-contain bg-bottom bg-blend-multiply">
+            <div className="w-full h-full flex flex-col md:flex-row justify-center items-center gap-4 relative">
                 <div className="md:basis-1/2 basis-full flex flex-col justify-items-center md:justify-items-start md:text-left gap-4">
-                    <h1 className="text-2xl capitalize">{props.pageTitle}</h1>
+                    <h1 className="text-4xl font-bold capitalize text-[var(--brand-color)]">{props.pageTitle}</h1>
                     <h2 className="text-lg">{props.pageSubTitle}</h2>
                     <p className="text-sm">{props.support}</p>
                     <div className="cta flex flex-col gap-2">
